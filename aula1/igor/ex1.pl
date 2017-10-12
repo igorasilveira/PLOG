7591c8d3cd96d4fef04663e344c8358938b3e3b9
IgorSilveira00 <up201505172@fe.up.pt>
2017-10-12 18:52:22 +0100

    aula 1 ex

---
+++ aula1/igor/ex1.pl
@@ -0,0 +1,20 @@
+male('Aldo Burrows').
+male('Lincoln Burrows').
+male('LJ Burrows').
+male('Michael Scofield').
+
+female('Lisa Rix').
+female('Christina Rose Scofield').
+female('Ella Scofield').
+female('Sara Tancradi').
+
+parent('Aldo Burrows', 'Lincoln Burrows').
+parent('Christina Rose Scofield', 'Lincoln Burrows').
+parent('Aldo Burrows', 'Michael Scofield').
+parent('Christina Rose Scofield', 'Michael Scofield').
+
+parent('Lisa Rix', 'LJ Burrows').
+parent('Lincoln Burrows', 'LJ Burrows').
+
+parent('Michael Scofield', 'Ella Scofield').
+parent('Sara Tancredi', 'Ella Scofield').
