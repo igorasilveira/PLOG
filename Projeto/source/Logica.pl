8f7ceb15fa69a8f39e5ac12ac46a6bddc32e2b27
Igor Silveira <up201505172@fe.up.pt>
2017-11-08 23:16:40 +0000

    dasdsa

--- Projeto/source/Logica.pl
+++ Projeto/source/Logica.pl
@@ -23,7 +23,8 @@ trocaJogador(2, NovoJogador) :-
 trataTopo([NumberReceived | T], TipoJogo, ModoJogadores, Tabuleiro, Jogador, J1Pontos, J2Pontos) :-
 	Tabuleiro2 = _,
 	char_code(Number1, NumberReceived),
-	number_chars(Number, Number1),
+	atom_chars(Number1, Number2),
+	number_chars(Number, Number2),
 	insereVertical(Tabuleiro, Tabuleiro2, 0, 0, Number, Jogador),
 	trocaJogador(Jogador, NovoJogador),
 	jogo(TipoJogo, ModoJogadores, Tabuleiro2, NovoJogador, J1Pontos, J2Pontos).
